15fc58659ca8154c33fc8e18c3fa883e82ade444
moisesJduarte <gladmd19@gmail.com>
2022-08-31 12:02:49 -0400

    rompiendo el loop

--- index.js
+++ index.js
@@ -16,21 +16,33 @@
         }
 
         let jugador = 0;
-        let pc = aleatorio(1,3)
-        jugador = prompt("Elige: 1 para piedra, 2 para papel y 3 para tijera");
-        
-        alert("PC elige: " + eleccion(pc))
-        alert("Tu eliges: " + eleccion(jugador))
-    // Combate
-        if(pc == jugador) {
-            alert("Empate")
-        } else if(jugador == 1 && pc == 3) {
-            alert("Ganaste")
-        } else if(jugador == 2 && pc == 1) {
-            alert("ganaste")
-        } else if(jugador == 3 && pc == 1){
-            alert("Perdiste")
-        } else {
-            alert("perdiste")
-        }
+        let pc = 0;
+        let triunfos = 0;
+        let perdidas = 0;
 
+        while (triunfos < 3 && perdidas < 3 ) {
+            pc = aleatorio(1,3)
+            jugador = prompt("Elige: 1 para piedra, 2 para papel y 3 para tijera");
+            
+            alert("PC elige: " + eleccion(pc))
+            alert("Tu eliges: " + eleccion(jugador))
+        // Combate
+            if(pc == jugador) {
+                alert("Empate")
+            } else if(jugador == 1 && pc == 3) {
+                alert("Ganaste")
+                triunfos = triunfos + 1
+            } else if(jugador == 2 && pc == 1) {
+                alert("ganaste")
+                triunfos = triunfos + 1
+            } else if(jugador == 3 && pc == 2){
+                alert("ganaste")
+                triunfos = triunfos + 1
+            } else {
+                alert("perdiste")
+                perdidas = perdidas + 1
+            }
+
+        }
+        
+        alert("Ganaste " + triunfos + " veces. Perdistes " + perdidas + " veces. ")
